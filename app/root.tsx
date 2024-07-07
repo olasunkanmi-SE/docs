ca980f175f3815394e691d31200bfef4c5fef3e2
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-07-07 11:52:47 +0800

    update the app to support dynamic contact rendering

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,21 @@
-import { Form, Links, Meta, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Form, Links, Meta, Scripts, ScrollRestoration, Outlet, Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
 import type { LinksFunction } from "@remix-run/node";
 
 import appStylesHref from "./app.css?url";
 
+import { ContactRecord, getContacts } from "./data";
+
+export const loader = async () => {
+  const contacts: ContactRecord[] = await getContacts();
+  return json({ contacts });
+};
+
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: appStylesHref }];
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -28,17 +37,33 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
