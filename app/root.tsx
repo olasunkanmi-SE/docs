4a34e183e7e0a35e991ca19e8a44bc826a0e533b
Olasunkanmi Oyinlola <olasunkanmioyinlola@Olasunkanmis-Mac-mini.local>
2024-07-07 19:18:37 +0800

    feat(ui): Add sidebar component and initial data
    Added a sidebar component with search functionality and a list of contacts
    Implemented initial data for contacts in data.ts file
    Introduced new dependencies: @remix-run/react and @remix-run/node
    Updated root.tsx file to use the new sidebar component

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,14 @@
+import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import {
-  Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
-  Outlet,
-  useNavigation,
   useLoaderData,
-  NavLink,
+  useNavigation,
   useSubmit,
 } from "@remix-run/react";
-import { json, redirect, LoaderFunctionArgs } from "@remix-run/node";
 import { useEffect } from "react";
 
 import type { LinksFunction } from "@remix-run/node";
@@ -19,8 +17,9 @@ import appStylesHref from "./app.css?url";
 
 import styles from "./style.css?url";
 
-import { getContacts, createEmptyContact } from "./data";
 import { NavBar } from "./components/navbar";
+import { SideBar } from "./components/sidebar";
+import { createEmptyContact, getContacts } from "./data";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
@@ -64,62 +63,7 @@ export default function App() {
       </head>
       <NavBar></NavBar>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form
-              id="search-form"
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch,
-                });
-              }}
-              role="search"
-            >
-              <input
-                id="q"
-                aria-label="Search contacts"
-                className={searching ? "loading" : ""}
-                defaultValue={q || ""}
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={!searching} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink
-                      className={({ isActive, isPending }) => (isActive ? "active" : isPending ? "pending" : "")}
-                      to={`contacts/${contact.id}`}
-                    >
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite ? <span>★</span> : null}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
+        <SideBar q={q} contacts={contacts} searching={searching} />
         <div className={navigation.state === "loading" && !searching ? "loading" : ""} id="detail">
           <Outlet />
         </div>
